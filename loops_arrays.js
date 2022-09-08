e7e36e5770837463a97f8d4399a304a5c673115d
GTzyxy <107081490+GTzyxy@users.noreply.github.com>
2022-09-08 18:25:28 -0300

    Add files via upload

---
+++ loops_arrays.js
@@ -0,0 +1,52 @@
+// //criando um array = uma possibilidade
+// //de armazenar uma lista dentro de uma variavel
+// var array = []
+// console.log(array);
+
+// //adicionando itens no array/lista
+// array = ["ovo", "farinha", "leite", "açucar"];
+// console.log(array);
+
+// //cumprimento do array
+// console.log(array.length);
+
+// //pegando um unico item da lista
+// console.log(array[2]);
+// //quero comprar mais item
+// console.log(array.push("cacau em pó"));
+// console.log(array);
+
+// //tirar um item da lista
+// console.log(array.pop());
+// console.log(array);
+
+// //desafio
+// //1. criar uma lista de cargos 3>= estagiario, analista, chefe
+// //2. exibir o analista
+// //3. contratar alguem => estagiario2
+
+// var funcionarios = ["estagiario", "analista", "chefe"]
+// console.log(funcionarios);
+
+// console.log(funcionarios[1]);
+
+// console.log(funcionarios.push("estagiario2"));
+// console.log(funcionarios);
+
+
+
+// for(var texto = 0; texto <= 10; texto++){
+//     console.log(texto);
+// }
+
+// for(var texto = 0; texto <= 10; texto++){
+//     console.log("Eu amo programar!!<3");
+// }
+
+// //incrementando de 2 em 2 ate 20
+// for(var i = 2; i <= 20; i += 2){
+//     console.log(i);
+// }
+
+
+// console.log(array.length -1);
